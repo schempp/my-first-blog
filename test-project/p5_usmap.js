ef4ecdeb1acf6b7831d7596c8a0664db858cafb7
Tim Schempp <schempp@rohan.sdsu.edu>
2017-12-09 16:17:09 -0800

    Add files via upload

---
+++ test-project/p5_usmap.js
@@ -0,0 +1,35 @@
+function setup() {
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', 'assets/map.svg');
+	xhr.setRequestHeader('Content-Type', 'image/svg+xml');
+	xhr.onload = function() {
+		if (xhr.status === 200) {
+			addSVG(xhr.responseXML.documentElement)
+		} else {
+			println('Request failed. Returned status of ' + xhr.status);
+		}
+	};
+	xhr.send();
+}
+		
+function addSVG(svg) {
+	var mySVG = createElement('div');
+	mySVG.position(0, 0);
+	mySVG.elt.append(svg);
+	var myCanvas = createCanvas();
+	myCanvas.position(0, 0);
+	myCanvas.size(mySVG.size().width, mySVG.size().height);
+	
+	var buttons = selectAll('.state');
+	
+	buttons.forEach(function(button, idx, array) {
+		button.mouseClicked(function() {
+			println(this.id() + " clicked");
+			alert(this.id() + " clicked");
+		});
+	});
+}
+
+function draw() {
+	noLoop();
+}
